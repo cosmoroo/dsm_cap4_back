5be0a15856cf9ca3c68903158f281ca8576effd2
dti <dti@LAB204-22>
2023-09-05 09:06:24 -0300

    Refatoração: Documentação do produto

--- produto/src/main/java/com/fatec/produto/model/Produto.java
+++ produto/src/main/java/com/fatec/produto/model/Produto.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+/**
+ * hewo
+ * @author ~w~
+ *
+ */
 @Entity
 public class Produto {
 	@Id
